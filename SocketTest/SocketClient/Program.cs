115c3f20a98be147a46e4d5a298da2ad41847ece
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-04-20 14:08:28 +0300

    TCP start

---
+++ SocketTest/SocketClient/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Program
+    {
+        
+        static int port = 2895; // порт сервера
+        static string address = "192.168.1.2";//"127.0.0.1"; // адрес сервера
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //try
+            //{
+            //    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            //    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //    // подключаемся к удаленному хосту
+            //    socket.Connect(ipPoint);
+            //    Console.Write("Введите сообщение:");
+            //    string message = Console.ReadLine();
+            //    byte[] data = Encoding.Unicode.GetBytes(message);
+            //    socket.Send(data);
+
+            //    // получаем ответ
+            //    data = new byte[256]; // буфер для ответа
+            //    StringBuilder builder = new StringBuilder();
+            //    int bytes = 0; // количество полученных байт
+
+            //    do
+            //    {
+            //        bytes = socket.Receive(data, data.Length, 0);
+            //        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            //    }
+            //    while (socket.Available > 0);
+            //    Console.WriteLine("ответ сервера: " + builder.ToString());
+
+            //    // закрываем сокет
+            //    socket.Shutdown(SocketShutdown.Both);
+            //    socket.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.Read();
+
+
+
+            TcpClient client = new TcpClient(address, port);
+
+            //клієнт вводить повідомлення,яке буде передано далі серверу.
+            Console.Write("Enter your message:");
+            string message = Console.ReadLine();
+
+            //перетворення в масив байт строкового повідомлення введеного клієнтом.
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            //для взаємодії з сервером визначається метод GetStream(),яктй повертає об"єкт NetworkStream,через даний об"єкт
+            //можна передавати повідомлення на сервер і отримувати повідомлення з серверу.
+            NetworkStream stream = client.GetStream();
+
+            try
+            {
+                // відправка повідомлення.Перший параметр-двійкові дані(що були перетворені вище),другий параметр-задає зсув
+                // масива байт,з якого починається передача(зазвичай нуль,бо масив байт відправляється повністю),
+                // третій -розмір двійкових даних(масив байт відправляється повністю).
+                stream.Write(data, 0, data.Length);
+
+                // отримання відповіді.Створємо масив байт,які отримує клієнт.
+                byte[] readingData = new byte[256];
+
+                //створення порожної строчки для запису туди вже перетворенимх даних.
+                string responseData = string.Empty;
+
+                StringBuilder completeMessage = new StringBuilder();
+
+                //кількість считаних байтів.
+                int numberbytes= 0;
+
+                //поки в потоці є дані(властивість DataAvailable равно true),відбувається поетапне формування
+                //отриманої строки з сервера за допомогою класу StringBuilder,тими даними,що були прочитані в буфер на кожній ітерації.
+                //Після завершення отримуємо готове строкове повідомлення.
+                do
+                {
+
+                    numberbytes = stream.Read(readingData, 0, readingData.Length);
+                    completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, numberbytes));
+                }
+                while (stream.DataAvailable);
+                
+                responseData = completeMessage.ToString();
+
+                //Отримання відповіді клієнтом.
+                Console.WriteLine(responseData);
+                
+            }
+            finally
+            {
+                //після завершення передачі чи отримання даних,поток і серверне з"єднання повинні бути закриті.
+                stream.Close();
+                client.Close();
+            }
+
+
+        }
+    }
+}
---
+++ SocketTest/SocketServer/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    class Program
+    {
+        //static int port = 2895; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            //    //Console.WriteLine("Hello World!");
+            //    String strHostName = string.Empty;
+            //    // Getting Ip address of local machine...
+            //    // First get the host name of local machine.
+            //    strHostName = Dns.GetHostName();
+            //    Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            //    // Then using host name, get the IP address list..
+            //    IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            //    var addr = ipEntry.AddressList.ToList();
+            //    addr.Add(IPAddress.Parse("127.0.0.1"));
+            //    Console.WriteLine("Оберіть IP address");
+            //    for (int i = 0; i < addr.Count; i++)
+            //    {
+            //        Console.WriteLine("{0}. {1} ", i + 1, addr[i].ToString());
+            //    }
+            //    Console.Write("->_");
+            //    int selectIP = int.Parse(Console.ReadLine());
+
+            //    // получаем адреса для запуска сокета
+            //    IPEndPoint ipPoint = new IPEndPoint(addr[selectIP - 1], port);
+            //    // получаем адреса для запуска сокета
+            //    //IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            //    // создаем сокет
+            //    Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //    try
+            //    {
+            //        // связываем сокет с локальной точкой, по которой будем принимать данные
+            //        listenSocket.Bind(ipPoint);
+
+            //        // начинаем прослушивание
+            //        listenSocket.Listen(10);
+
+            //        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            //        while (true)
+            //        {
+            //            Socket handler = listenSocket.Accept();
+            //            // получаем сообщение
+            //            StringBuilder builder = new StringBuilder();
+            //            int bytes = 0; // количество полученных байтов
+            //            byte[] data = new byte[256]; // буфер для получаемых данных
+
+            //            do
+            //            {
+            //                bytes = handler.Receive(data);
+            //                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            //            }
+            //            while (handler.Available > 0);
+
+            //            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+            //            // отправляем ответ
+            //            string message = "ваше сообщение доставлено";
+            //            data = Encoding.Unicode.GetBytes(message);
+            //            handler.Send(data);
+            //            // закрываем сокет
+            //            handler.Shutdown(SocketShutdown.Both);
+            //            handler.Close();
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+
+
+
+            IPAddress localAddr = IPAddress.Parse("192.168.1.2");
+            int port = 2895;
+            TcpListener server = new TcpListener(localAddr, port);            
+            server.Start();
+            // 
+            while (true)
+            {
+                try
+                {
+                    // Подключение клиента
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    // Обмен данными
+                    try
+                    {
+                        if (stream.CanRead)
+                        {
+                            byte[] myReadBuffer = new byte[1024];
+                            StringBuilder myCompleteMessage = new StringBuilder();
+                            int numberOfBytesRead = 0;
+                            do
+                            {
+                                numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                                myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(myReadBuffer, 0, numberOfBytesRead));
+                            }
+                            while (stream.DataAvailable);
+                            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + myCompleteMessage.ToString());
+                            byte[] responseData = Encoding.UTF8.GetBytes("УСПЕШНО!");
+                            stream.Write(responseData, 0, responseData.Length);
+                        }
+                    }
+                    finally
+                    {
+                       // stream.Close();
+                       // client.Close();
+                    }
+                }
+                catch
+                {
+                    server.Stop();
+                    break;
+                }
+            }
+
+
+
+
+        }
+    
+    }
+}
