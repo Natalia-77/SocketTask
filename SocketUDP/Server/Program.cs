97a135e293edeff41f6c4ed8a86c54b1e59ed175
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-04-20 23:32:09 +0300

    Start udp

---
+++ SocketUDP/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //у вікні клієнта вводяться імена,прописані нижче і після цього виведеться розширена інфо по них ,
+            //отримана від сервера.
+            UdpClient udpc = new UdpClient(2055);
+            Console.WriteLine("Server started!");
+            IPEndPoint ep = null;
+            while (true)
+            {
+                byte[] rdata = udpc.Receive(ref ep);
+                string name = Encoding.ASCII.GetString(rdata);
+                string Info = string.Empty;
+                if (name == "Ola")  Info = "Her name is Ola";
+                if (name == "Valia") Info = "Her name is Valia";             
+                byte[] sdata = Encoding.ASCII.GetBytes(Info);
+                udpc.Send(sdata, sdata.Length, ep);
+            }
+        }
+    }
+}
---
+++ SocketUDP/SocketTestUDP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            UdpClient udpc = new UdpClient("127.0.0.1", 2055);
+            IPEndPoint ep = null;
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if (name == "") break;
+                byte[] sdata = Encoding.ASCII.GetBytes(name);
+                udpc.Send(sdata, sdata.Length);
+                byte[] rdata = udpc.Receive(ref ep);
+                string info = Encoding.ASCII.GetString(rdata);
+                Console.WriteLine(info);
+            }
+        }
+    }
+}
